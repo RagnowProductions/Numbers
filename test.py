274424259371ffdae2845be42018a914c1dd5058
Fenn <nicolaileff@icloud.com>
2023-03-11 17:42:38 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 while True:
-  if type == "+"
-  *insert here*
+  if type == "+":
+    if a == num + 1:
+        num = a + 1
+  elif type == "-":
+    if a == num - 1:
+        num = a - 1
+  elif type == "*":
+    if a == num * 2:
+        num = a * 2 
+  elif type == "/":
+    if a == num / 1:
+        num = a / 1
