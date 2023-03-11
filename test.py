c9e8bcaac7aac208e2f30dd92cc3b7e2885117d5
Fenn <nicolaileff@icloud.com>
2023-03-11 17:44:33 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
+import math
+import time
+print("A FENNGAMING ORIGINAL")
+time.sleep(1)
+print("FINALLY MADE IN PYTHON INSTEAD OF SCRATCH")
+time.sleep(3)
+print("FENNGAMING NUMBERS GAME.PYTHON")
+time.sleep(1)
+print("PRODUCED BY RAGNOWPRODUCTIONS 2023")
+time.sleep(1)
+print("RULES: TYPE THE NUMBER AFTER THE VARIABLE BEFORE (WE'LL GIVE YOU A NUMBER AND YOU HAVE TO ADD 1) ")
+time.sleep(1)
+print("STARTING...!")
+time.sleep(5)
+num = 0
 while True:
   a = int(input(num))
   if type == "+":
@@ -12,3 +27,4 @@ while True:
   elif type == "/":
     if a == num / 1:
         num = a / 1
+  time.sleep(1)
