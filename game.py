86174ace9094b25ddb7b7b0e4ccc2c71405509d7
Fenn <nicolaileff@icloud.com>
2023-03-11 18:13:11 -0600

    Update and rename test.py to game.py

--- game.py
+++ game.py
@@ -8,7 +8,18 @@ print("FENNGAMING NUMBERS GAME.PYTHON")
 time.sleep(1)
 print("PRODUCED BY RAGNOWPRODUCTIONS 2023")
 time.sleep(1)
-print("RULES: TYPE THE NUMBER AFTER THE VARIABLE BEFORE (WE'LL GIVE YOU A NUMBER AND YOU HAVE TO ADD 1) ")
+type = input("OPERATION OF +-/*:")
+time.sleep(1)
+print("RULES:")
+time.sleep(1)
+if type == "+":
+  print("ADD 1 TO THE NUMBER WE GIVE YOU")
+elif type == "-":
+  print("SUPTRACT 1 TO THE NUMBER WE GIVE YOU") 
+elif type == "*":
+  print("MULTIPLY 2 TO THE NUMBER WE GIVE YOU") 
+elif type == "/":
+  print("DIVIDE 2 TO THE NUMBER WE GIVE YOU") 
 time.sleep(1)
 print("STARTING...!")
 time.sleep(5)
@@ -25,6 +36,6 @@ while True:
     if a == num * 2:
         num = a * 2 
   elif type == "/":
-    if a == num / 1:
-        num = a / 1
+    if a == num / 2:
+        num = a / 2
   time.sleep(1)
